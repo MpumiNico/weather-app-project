c4306f4b7381a0315779873c46d4c3e654d3251e
MpumiNico <142667245+MpumiNico@users.noreply.github.com>
2024-11-08 11:52:10 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Pretoria" />
+        <footer>
+          This project is was coded by{" "}
+          <a href="https://github.com/MpumiNico">Mpumi Nicollete Mahlalela</a> and is{" "}
+          <a href="https://github.com/MpumiNico/weather-app">
+            open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://netlify.app/">Netlify</a>
+        </footer>
+      </div>
+    </div>
+  );
+}
